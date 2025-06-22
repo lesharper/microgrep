4cb127ef8ed1edaa10f5f8738b82ee837eb7862e
Алексей Новичкин <a.novichkin@norder.me>
2025-06-22 19:12:28 +0800

    refactory modularity

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use std::env;
 use std::fs;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args);
-
-    println!("В файле {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path).expect("Ошибка чтения файла");
-
-    println!("Текст:\n{contents}");
-}
+use std::process;
 
 struct Config {
     query: String,
@@ -19,10 +8,35 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Проблема с парсингом: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    run(config);
 }
