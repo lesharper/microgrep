9aebc67afab3022bd075c5b73253e03932a22a63
Алексей Новичкин <a.novichkin@norder.me>
2025-06-22 17:42:33 +0800

    switch function parse -> method for config struct (idiomatic)

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("В файле {}", config.file_path);
-    
+
     let contents = fs::read_to_string(config.file_path).expect("Ошибка чтения файла");
-    
+
     println!("Текст:\n{contents}");
-
 }
-
 
 struct Config {
     query: String,
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
