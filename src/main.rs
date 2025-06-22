1d5e76887fdee71d8b1c9393678af2430e90e302
Алексей Новичкин <a.novichkin@norder.me>
2025-06-22 17:37:50 +0800

    add struct, add test file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-    
-    println!("Searching for {query}", );
-    println!("In file {file_path}");
+    let config = parse_config(&args);
 
+    println!("В файле {}", config.file_path);
+    
+    let contents = fs::read_to_string(config.file_path).expect("Ошибка чтения файла");
+    
+    println!("Текст:\n{contents}");
+
+}
+
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
